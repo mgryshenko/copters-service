465698e96afb00264299b944491d034aa1ebab28
Mykhailo Gryshchenko <mgryshenko@gmail.com>
2022-10-28 17:37:13 +0300

    Initial commit

---
+++ src/main/java/com/mgryshenko/coptersservice/CoptersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mgryshenko.coptersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoptersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoptersServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgryshenko/coptersservice/CoptersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgryshenko.coptersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoptersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
